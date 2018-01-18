aa39868d903c32874b8b68c938d9ef36087e2cf6
Kurilo Denis <kdb2015@mail.ru>
2018-01-18 17:27:36 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
-$(document).ready(function(){
+// Переменные
+	$(document).ready(function(){
+		var pull = $("#navigation-toggle");
+		var nav = $("#mobile-nav");
+		//События при нажатии на кнопку
+		$(pull).on("click", function(e){
+			//Отменяет стандартное поведение в браузере 
+			e.preventDefault();
+			//Открываем или скрываем меню
+			$(nav).slideToggle();
 
-
-
-});
+			//Добавляем модификатор --active
+			$(this).toggleClass('.navigation__togle-button--active');
+		});
+		//При изменении размера окна, в большую сторону, если меню было скрыто показывает его.
+		$(window).resize(function(){
+			var w = $(window).width();
+			if(w > 720 && nav.is(':hidden')){
+				nav.removeAttr('style');
+			}
+		});	
+	});
